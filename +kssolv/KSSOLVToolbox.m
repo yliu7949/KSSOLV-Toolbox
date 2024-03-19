c960db15e10ed881fe30eb5ba1b6bb6affddb11c
Liu Yang <yliu7949@gmail.com>
2024-03-19 15:02:26 +0800

    fix(): typo and resources

--- +kssolv/KSSOLVToolbox.m
+++ +kssolv/KSSOLVToolbox.m
@@ -14,7 +14,7 @@ classdef KSSOLVToolbox < handle
         function this = KSSOLVToolbox()
             %KSSOLVTOOLBOX 构造此类的实例
             import kssolv.ui.util.Localizer.*
-            % setLocale('en_US');
+            setLocale('en_US');
             % App 标题
             title = message('KSSOLV:toolbox:AppTitle');
             % 创建 App Container
