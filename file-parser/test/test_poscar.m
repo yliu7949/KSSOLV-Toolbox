7e0d12d957bb58f575b6c06dfbf22a0fb80eabce
yliu7949 <yliu7949@gmail.com>
2024-05-03 00:33:52 +0800

    chore(file-parser): remove file-parser directory

--- file-parser/test/test_poscar.m
+++
@@ -1,8 +0,0 @@
-clear;
-%Test1: Si8
-% args=struct('POSCAR','Si.vasp/POSCAR');
-% mol=readPOSCAR(args);
-
-%Test2: Al2O3
-args=struct('POSCAR','Al2O3.vasp/POSCAR');%,'KPOINTS','Si.vasp/KPOINTS','INCAR','Si.vasp/INCAR');
-cry=readPOSCAR(args);
